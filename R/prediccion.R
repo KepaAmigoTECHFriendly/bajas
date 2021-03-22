184c6ea4e2d8cb9b951f241df7bb177599234cdc
KepaAmigoTECHFriendly <58561058+KepaAmigoTECHFriendly@users.noreply.github.com>
2021-03-22 10:23:57 +0100

    Add files via upload

--- R/prediccion.R
+++ R/prediccion.R
@@ -2,17 +2,17 @@
 #'
 #' @description Recibe un registro en JSON y devuleve una predicción del mismo en json
 #'
-#' @param registro
+#' @param ContadorBajasCCC, anyconti, numconti, ContadordiasBajasDNI, sexo, cnae, fecha.baja, fecha.alta, icd9, recaida, num.trabajadores.emp, codigo.postal, fecha.alta.empresa, contrato, grupo.cotizacion, pluriempleo, diasemana, esprincipal, fecha.nacimiento
 #'
 #' @return json
 #'
-#' @examples  prediccion(5)
+#' @examples  prediccion("8","2010","29754A","78","1","8122","15/12/2016","-","727.61","","7","25600","12/4/1999","100","8","","jue.","1","10/5/1958")
 #'
 #' @import jsonlite
 #'
 #' @export
 
-prediccion <- function(registro){
+prediccion <- function(ContadorBajasCCC, anyconti, numconti, ContadordiasBajasDNI, sexo, cnae, fecha.baja, fecha.alta, icd9, recaida, num.trabajadores.emp, codigo.postal, fecha.alta.empresa, contrato, grupo.cotizacion, pluriempleo, diasemana, esprincipal, fecha.nacimiento){
   json <- paste('{"rango":[',0,',',1,'],"probabilidad":',0.75,'}',sep = "")
   return(json)
 }
